e67f425a9d60177e23049f41c926b34ad4ff4b77
sepehr ansaripour <pehrans@gmail.com>
2021-09-05 14:42:48 -0400

    :recycle: Configify some Tailwind values

--- src/cljs/nw_calculator/styles.cljs
+++ src/cljs/nw_calculator/styles.cljs
@@ -2,45 +2,69 @@
   (:require [cljstache.core :as fmt]
             [spade.core :as spade]))
 
-(def color-palette {:purple {:hex "#9b4dca" :rgb "155,77,202"}})
+(def config
+  {:colors {:purple {:hex "#9b4dca" :rgb "155,77,202"}}
+   :sizes  {:screen   {:md "768px"}
+            :relative {:0   "0rem"
+                       :1   "0.25rem"
+                       :2   "0.5rem"
+                       :4   "1rem"
+                       :11  "2.75rem"
+                       :12  "3rem"
+                       :96  "24rem"
+                       :152 "40rem"
+                       :136 "32rem"}}})
 
-(defn with-color [s] (fmt/render s color-palette))
+(defn with-cfg [s] (fmt/render s config))
 
-(def md "768px")
+(def relative-0
+  (-> config :sizes :relative :0))
+(def relative-1
+  (-> config :sizes :relative :1))
+(def relative-2
+  (-> config :sizes :relative :2))
+(def relative-12
+  (-> config :sizes :relative :12))
+(def relative-96
+  (-> config :sizes :relative :96))
+(def relative-136
+  (-> config :sizes :relative :136))
+(def relative-152
+  (-> config :sizes :relative :152))
 
 (spade/defglobal global-styles
-  (at-media {:min-width md}
-            [".md\\:w-152-imp" {:width "40rem !important"}]
-            [".md\\:pr-12-imp" {:padding-right "3rem !important"}])
+  (at-media {:min-width (-> config :sizes :screen :md)}
+            [".md\\:w-152-imp" {:width (with-cfg "{{sizes.relative.152}} !important")}]
+            [".md\\:pr-12-imp" {:padding-right (with-cfg "{{sizes.relative.12}} !important")}])
   [:body {:font-family "'Roboto', sans-serif"}]
-  [:button.button {:padding "0px 1rem 0px 1rem"}]
-  [:.border-purple {:border-color (with-color "rgba({{purple.rgb}}, var(--tw-border-opacity))")}]
-  [:.bg-purple {:background-color (with-color "rgba({{purple.rgb}}, var(--tw-bg-opacity))")}]
-  [:.text-purple {:color (with-color "rgba({{purple.rgb}}, var(--tw-text-opacity))")}]
-  [:.m-0-imp {:margin "0px !important"}]
-  [:.min-w-12 {:min-width :3rem}]
-  [:.min-w-96 {:min-width :24rem}]
-  [:.w-152 {:width :40rem}]
-  [:.w-136 {:width :32rem}]
-  [:.w-11-imp {:width "2.75rem !important"}]
-  [:input.basic-input {:padding-bottom :0px
-                       :padding-top    :0px
-                       :padding-left   :.25rem
-                       :height         :3rem
+  [:button.button {:padding (with-cfg "0px {{sizes.relative.4}} 0px {{sizes.relative.4}}")}]
+  [:.border-purple {:border-color (with-cfg "rgba({{colors.purple.rgb}}, var(--tw-border-opacity))")}]
+  [:.bg-purple {:background-color (with-cfg "rgba({{colors.purple.rgb}}, var(--tw-bg-opacity))")}]
+  [:.text-purple {:color (with-cfg "rgba({{colors.purple.rgb}}, var(--tw-text-opacity))")}]
+  [:.m-0-imp {:margin (with-cfg "{{sizes.relative.0}} !important")}]
+  [:.w-11-imp {:width (with-cfg "{{sizes.relative.11}} !important")}]
+  [:.min-w-12 {:min-width relative-12}]
+  [:.min-w-96 {:min-width relative-96}]
+  [:.w-136 {:width relative-136}]
+  [:.w-152 {:width relative-152}]
+  [:input.basic-input {:padding-bottom relative-0
+                       :padding-top    relative-0
+                       :padding-left   relative-1
+                       :height         relative-12
                        :border-width   :2px
-                       :border-top     :0px
-                       :border-left    :0px
-                       :border-right   :0px
-                       :border-radius  :0px}]
+                       :border-top     relative-0
+                       :border-left    relative-0
+                       :border-right   relative-0
+                       :border-radius  relative-0}]
   ["input::-webkit-outer-spin-button, input::-webkit-inner-spin-button"
    {:-webkit-appearance :none
-    :margin             :0px}]
+    :margin             relative-0}]
   ["input[type=number]" {:-moz-appearance :textfield}])
 
 (spade/defclass search-result-item-class []
   [:.labels {:width          :100%
              :flex-direction :column
              :align-items    :start
-             :gap            :.5rem}])
+             :gap            relative-2}])
 
 
