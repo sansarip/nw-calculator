109832079978cae90da6a251413aff7e5142719d
sepehr ansaripour <pehrans@gmail.com>
2021-09-09 20:09:52 -0400

    :lipstick: Increase card bottom padding on smaller screen sizes

--- src/cljs/nw_calculator/components/card/component.cljs
+++ src/cljs/nw_calculator/components/card/component.cljs
@@ -1,3 +1,3 @@
 (ns nw-calculator.components.card.component)
 
-(def card :div.relative.bg-white.h-auto.p-5.pt-16.md:px-10.md:pb-10.w-128.md:w-176-imp.rounded.shadow-lg)
+(def card :div.relative.bg-white.h-auto.px-5.pb-8.pt-16.md:px-10.md:pb-10.w-128.md:w-176-imp.rounded.shadow-lg)
