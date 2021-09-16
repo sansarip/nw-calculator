b0d8281faf7a44b696ba2e8cf882147de16a66c9
sepehr ansaripour <pehrans@gmail.com>
2021-09-16 15:54:36 -0400

    ⚰️ Remove unused local-storage references

--- src/cljs/nw_calculator/utilities.cljs
+++ src/cljs/nw_calculator/utilities.cljs
@@ -84,9 +84,3 @@
   (let [left (subvec v 0 n)
         right (subvec v (inc n))]
     (vec (concat left right))))
-
-(defn get-local-storage [k]
-  (edn/read-string (js/window.localStorage.getItem k)))
-
-(defn set-local-storage! [k v]
-  (js/window.localStorage.setItem k v))
