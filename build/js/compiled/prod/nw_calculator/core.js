e7cb60f2c67323e6e4e39fa57a4bae0fbf2f3787
sepehr ansaripour <pehrans@gmail.com>
2021-11-26 18:49:46 -0500

    :bookmark: Release 2021.11.26.3

--- build/js/compiled/prod/nw_calculator/core.js
+++ build/js/compiled/prod/nw_calculator/core.js
@@ -12,9 +12,9 @@ goog.require('nw_calculator.defaults');
 nw_calculator.core.dev_mode_BANG_ = (function nw_calculator$core$dev_mode_BANG_(){
 cljs.core.enable_console_print_BANG_();
 
-return taoensso.timbre._log_BANG_.cljs$core$IFn$_invoke$arity$11(taoensso.timbre._STAR_config_STAR_,cljs.core.cst$kw$info,"nw-calculator.core","/private/var/folders/sb/g6t896vs227cr9lgx60z75jc0000gp/T/form-init17861542048565195405.clj",13,cljs.core.cst$kw$p,cljs.core.cst$kw$auto,(new cljs.core.Delay((function (){
+return taoensso.timbre._log_BANG_.cljs$core$IFn$_invoke$arity$11(taoensso.timbre._STAR_config_STAR_,cljs.core.cst$kw$info,"nw-calculator.core","/private/var/folders/sb/g6t896vs227cr9lgx60z75jc0000gp/T/form-init9503675525543355075.clj",13,cljs.core.cst$kw$p,cljs.core.cst$kw$auto,(new cljs.core.Delay((function (){
 return new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, ["\uD83D\uDC77 Dev mode!"], null);
-}),null)),null,-2121792488,null);
+}),null)),null,-2002977181,null);
 });
 nw_calculator.core.initialize_app_BANG_ = (function nw_calculator$core$initialize_app_BANG_(){
 re_frame.core.clear_subscription_cache_BANG_();
