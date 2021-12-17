78ddff364790a8254b56e7b048b16fa8a1626fe8
sepehr ansaripour <pehrans@gmail.com>
2021-12-16 21:45:16 -0500

    :bug: Add missing enlive dep

--- project.clj
+++ project.clj
@@ -76,6 +76,7 @@
    [[clj-http "3.12.3"]
     [cheshire "5.10.0"]
     [progrock "0.1.2"]
-    [slingshot "0.12.2"]],
+    [slingshot "0.12.2"]
+    [enlive "1.1.6"]],
    :main nw-calculator.scraper.main,
    :repl-options {:init-ns nw-calculator.scraper.main}}})
